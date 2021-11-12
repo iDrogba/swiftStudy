6c85e6ec267ca251b77bd2cbba26c31d6e6da054
KimSangHyun <kshy1019@naver.com>
2021-11-12 09:35:22 +0900

    init

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by 김상현 on 2021/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController,EditDelegate {
+    
+    let imgOn = UIImage(named: "1.jpeg")
+    let imgOff = UIImage(named: "2.jpeg")
+    
+    var isOn = true
+
+    @IBOutlet var txtMessage: UITextField!
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imgView.image = imgOn
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+            editViewController.textWayValue = "segue : use button"
+        }else if segue.identifier == "editBarButton" {
+            editViewController.textWayValue = "segue : use Bar button"
+        }
+        editViewController.textMessage = txtMessage.text!
+        editViewController.isOn = isOn
+        editViewController.delegate = self
+    }
+    
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txtMessage.text = message
+    }
+    
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool) {
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        }else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
+    }
+    
+}
+
